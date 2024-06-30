076150bbc6bb7f14d8d5e9b50923eccd81c45bdf
Hala Mansour <halamansour888222555@gmail.com>
2024-06-30 14:39:36 +0300

    update cors

--- ShippingSystem.PL/Program.cs
+++ ShippingSystem.PL/Program.cs
@@ -71,8 +71,8 @@ namespace ShippingSystem.PL
 
             //Register Emp Services 
             builder.Services.AddScoped<IGenericRepository<Account>, GenericRepository<Account>>();
-            builder.Services.AddScoped<IGenericRepository<Role>, GenericRepository<Role>>();
             builder.Services.AddScoped<IGenericStatusRepository<Branch>, GenericStatusRepository<Branch>>();
+            //builder.Services.AddScoped<IGenericStatusRepository<Government>, GenericStatusRepository<Government>>();
             builder.Services.AddScoped<IGenericRepository<ExistedEntities>, GenericRepository<ExistedEntities>>();
 
             //Delivery Accounts
@@ -80,7 +80,7 @@ namespace ShippingSystem.PL
 
             //builder.Services.AddScoped<IGenericRepository<Permission_User_Entities>, GenericRepository<Permission_User_Entities>>();
             builder.Services.AddScoped<EmployeeService>();
-            builder.Services.AddScoped<RoleService>();
+            //builder.Services.AddScoped<PermissionsService>();
 
             // Delivery Accounts Service
             builder.Services.AddScoped<DeliveryAccountService>();
@@ -95,17 +95,25 @@ namespace ShippingSystem.PL
 
 
 
-            // Allow Cors
-            string txt = "AllowedURLS";
-            builder.Services.AddCors(o =>
+
+            //  add  CORS configuration:
+
+            builder.Services.AddCors(options =>
             {
-                o.AddPolicy(txt, builder =>
+                options.AddDefaultPolicy(builder =>
                 {
-                    builder.AllowAnyOrigin();
-                    builder.AllowAnyMethod();
-                    builder.AllowAnyHeader();
+                    builder.WithOrigins("http://localhost:4200")
+                           .AllowAnyMethod()
+                           .AllowAnyHeader();
                 });
             });
+
+
+
+
+
+
+
 
             var app = builder.Build();
 
@@ -117,9 +125,7 @@ namespace ShippingSystem.PL
             }
 
             app.UseHttpsRedirection();
-
-            app.UseCors(txt);
-
+            app.UseCors();
             app.UseAuthorization();
 
 
