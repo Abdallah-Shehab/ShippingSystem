b4fdb5fa657fb930c72d543646fe9bbc689191d8
Hala Mansour <halamansour888222555@gmail.com>
2024-07-01 15:50:57 +0300

    Display Merchant Accounts

---
+++ ShippingSysem.BLL/Services/MerchantAccountService.cs
@@ -0,0 +1,50 @@
+﻿using ShippingSysem.BLL.DTOs.DeliveryDTOS;
+using ShippingSysem.BLL.DTOs.MerchantDTOS;
+using ShippingSystem.DAL.Interfaces.Base;
+using ShippingSystem.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingSysem.BLL.Services
+{
+    public class MerchantAccountService
+    {
+        private readonly IGenericRepository<MerchantAccount> genRepo;
+        public MerchantAccountService(IGenericRepository<MerchantAccount> genRepo) {
+            this.genRepo = genRepo;
+        }
+
+
+
+
+        //method to Display All Merchant Accounts
+        public async Task<List<DisplayMerchantAccountsDTO>> GetAllMerchantAccounts()
+        {
+            var accounts = await genRepo.GetAllAsync();
+            var dtos = accounts
+                .Select(acc => new DisplayMerchantAccountsDTO
+                {
+                    ID=acc.Id,
+                    Name = acc.Name,
+                    email = acc.Email,
+                    password = acc.PasswordHash,
+                    Branch = acc.Branch.Name,
+                    Address = acc.Address,
+                    StoreName = acc.StoreName,
+                    Government = acc.Government,
+                    City = acc.City,
+                    Pickup_Price = acc.Pickup_Price,
+                    Refund_Percentage = acc.Id,
+
+                })
+                .ToList();
+
+            return dtos;
+        }
+
+
+    }
+}
---
+++ ShippingSystem.PL/Controllers/MerchantController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShippingSysem.BLL.Services;
+
+namespace ShippingSystem.PL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MerchantController : ControllerBase
+    {
+
+        private readonly MerchantAccountService _merchantAccountService;
+
+        public MerchantController(MerchantAccountService _merchantAccountService)
+        {
+            this._merchantAccountService = _merchantAccountService;
+        }
+
+
+        //Action to display Merchant Accounts
+        [HttpGet]
+        public async Task<IActionResult> GetAllMerchantAccounts()
+        {
+            var accounts = await _merchantAccountService.GetAllMerchantAccounts();
+            return Ok(accounts);
+        }
+
+
+    }
+}
