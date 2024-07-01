d1522b734c53ff51747fbd878d776382b182fa58
Hala Mansour <halamansour888222555@gmail.com>
2024-07-01 16:34:11 +0300

    Add , update ,Delete , get by id Merchant Account

--- ShippingSysem.BLL/DTOs/MerchantDTOS/AddMerchantAccountDTO.cs
+++ ShippingSysem.BLL/DTOs/MerchantDTOS/AddMerchantAccountDTO.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ShippingSystem.DAL.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace ShippingSysem.BLL.DTOs.MerchantDTOS
 {
-    internal class AddMerchantAccountDTO
+    public class AddMerchantAccountDTO
     {
+        [Required(ErrorMessage = "ID is required")]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [MinLength(3, ErrorMessage = "Name must be at least 3 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Branch is required")]
+        public int BranchId { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Store name is required")]
+        public string StoreName { get; set; }
+
+        [Required(ErrorMessage = "Government is required")]
+        public string Government { get; set; }
+
+        [Required(ErrorMessage = "City is required")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Pickup price is required")]
+        [Range(0, double.MaxValue, ErrorMessage = "Pickup price must be a positive value")]
+        public decimal Pickup_Price { get; set; }
+
+        [Required(ErrorMessage = "Refund percentage is required")]
+        [Range(0, 100, ErrorMessage = "Refund percentage must be between 0 and 100")]
+        public decimal Refund_Percentage { get; set; }
     }
 }
--- ShippingSysem.BLL/Services/MerchantAccountService.cs
+++ ShippingSysem.BLL/Services/MerchantAccountService.cs
@@ -46,5 +46,106 @@ namespace ShippingSysem.BLL.Services
         }
 
 
+
+
+        //method to Add Merchant Account
+        public async Task<bool> AddMerchantAccount(AddMerchantAccountDTO dto)
+        {
+            try
+            {
+                var account = new MerchantAccount
+                {
+                    Name = dto.Name,
+                    Email = dto.Email,
+                    PasswordHash = dto.Password,
+                    BranchID = dto.BranchId,
+                    Address = dto.Address,
+                    StoreName = dto.StoreName,
+                    Government = dto.Government,
+                    City = dto.City,
+                    Pickup_Price = dto.Pickup_Price,
+                    Refund_Percentage = dto.Refund_Percentage,
+                    UserName = dto.Name 
+                };
+
+                await genRepo.AddAsync(account);
+                await genRepo.SaveAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+
+        // Method to delete merchant account
+        public async Task<bool> DeleteMerchantAccount(int accountId)
+        {
+            try
+            {
+                var account = await genRepo.GetByIdAsync(accountId);
+                if (account == null)
+                {
+                    return false;
+                }
+
+                await genRepo.DeleteById(accountId);
+                await genRepo.SaveAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+
+
+        // Method to get merchant account by ID
+        public async Task<MerchantAccount> GetMerchantAccountById(int id)
+        {
+            return await genRepo.GetByIdAsync(id);
+        }
+
+
+
+
+
+        // Method to update merchant account
+        public async Task<bool> UpdateMerchantAccount(int accountId, AddMerchantAccountDTO dto)
+        {
+            try
+            {
+                var existingAccount = await genRepo.GetByIdAsync(accountId);
+                if (existingAccount == null)
+                {
+                    return false;
+                }
+
+                // Update properties
+                existingAccount.Name = dto.Name;
+                existingAccount.Email = dto.Email;
+                existingAccount.PasswordHash = dto.Password;
+                existingAccount.BranchID = dto.BranchId;
+                existingAccount.Address = dto.Address;
+                existingAccount.StoreName = dto.StoreName;
+                existingAccount.Government = dto.Government;
+                existingAccount.City = dto.City;
+                existingAccount.Pickup_Price = dto.Pickup_Price;
+                existingAccount.Refund_Percentage = dto.Refund_Percentage;
+                existingAccount.UserName = dto.Name;
+
+                await genRepo.Update(existingAccount);
+                await genRepo.SaveAsync();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
     }
 }
