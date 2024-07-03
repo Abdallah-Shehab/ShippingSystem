a55f728902d60cfdd57aef195eb6bcce3ae3618f
MohamedHamdy <mohahamdy11@gmail.com>
2024-07-03 20:42:33 +0300

    add end point to return all orders depend on merchant id

--- ShippingSystem.PL/Controllers/OrdersController.cs
+++ ShippingSystem.PL/Controllers/OrdersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using ShippingSysem.BLL.DTOs.OrderDTOs;
 using ShippingSysem.BLL.Services;
+using ShippingSystem.DAL.Models;
 
 namespace ShippingSystem.PL.Controllers
 {
@@ -17,7 +18,7 @@ namespace ShippingSystem.PL.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAll(string status = "") {
+        public async Task<ActionResult<List<OrederReadDTO>>> GetAll(string status = "") {
             var orders = await orderService.GetAllFilterdOrders(status);
 
             if (!orders.Any())
@@ -27,7 +28,7 @@ namespace ShippingSystem.PL.Controllers
         }
 
         [HttpGet("AllWithPagination")]
-        public async Task<IActionResult> GetAllWithPagination(int page = 1,int pageSize = 10,string status="")
+        public async Task<ActionResult<List<OrederReadDTO>>> GetAllWithPagination(int page = 1,int pageSize = 10,string status="")
         {
             var orders = await orderService.GetAllFilterdOrders(page,pageSize,status);
             
@@ -38,27 +39,25 @@ namespace ShippingSystem.PL.Controllers
         }
 
         [HttpGet("OrdersCount")]
-        public async Task<IActionResult> GetOrdersCount(int merchantId = 0)
+        public async Task<ActionResult<List<OrderCount>>> GetOrdersCount(int merchantId = 0)
         {
-            if(merchantId == 0)
-            {
-                var orderCounts = await orderService.GetOrderCountsAsync();
-                
-                if (!orderCounts.Any())
-                    return NotFound();
-
-                return Ok(orderCounts);
-            }
-            else
-            {
                 var orderCounts = await orderService.GetOrderCountsAsync(merchantId);
 
                 if (!orderCounts.Any())
                     return NotFound();
 
                 return Ok(orderCounts);
-            }
-                
+        }
+
+        [HttpGet("AllOrdersForMerchant")]
+        public async Task<ActionResult<List<OrederReadDTO>>> GetAllForMerchant(string status="",int MerchantId = 0)
+        {
+            var orders = await orderService.GetAllOrdersForMerchant(status, MerchantId);
+
+            if (!orders.Any())
+                return NotFound();
+
+            return Ok(orders);
         }
     }
 }
