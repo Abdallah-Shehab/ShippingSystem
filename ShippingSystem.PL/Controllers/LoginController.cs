754c9be0c94e288c9a0247c469e51f0f8e249904
Abdullah SHafiQ <83695936+Abdallah-Shehab@users.noreply.github.com>
2024-07-08 14:08:45 +0300

    nothing changed

--- ShippingSystem.PL/Controllers/LoginController.cs
+++ ShippingSystem.PL/Controllers/LoginController.cs
@@ -7,42 +7,42 @@ using ShippingSystem.DAL.Models;
 using ShippingSystem.DAL.Models.Base;
 namespace ShippingSystem.PL.Controllers
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class LoginController : ControllerBase 
-	{
-		
-		public readonly GenericLoginService<Account> accountLoginService;
-		public readonly GenericLoginService<MerchantAccount> merchantLoginService;
-		public readonly GenericLoginService<DeliveryAccount> deliveryLoginService;
-		public LoginController(GenericLoginService<Account> accountLoginService,
-								GenericLoginService<MerchantAccount> merchantLoginService,
-								GenericLoginService<DeliveryAccount> deliveryLoginService)
-		{
-			this.accountLoginService = accountLoginService;
-			this.merchantLoginService = merchantLoginService;
-			this.deliveryLoginService = deliveryLoginService;
-		}
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
 
-		[HttpPost("Employee")]
-		public async Task<IActionResult> LoginEmployee(CreateLoginDTO createLoginDTO)
-		{
-			return  Ok(await accountLoginService.getTokenEmployee(createLoginDTO, account => account.Role));
-		}
+        public readonly GenericLoginService<Account> accountLoginService;
+        public readonly GenericLoginService<MerchantAccount> merchantLoginService;
+        public readonly GenericLoginService<DeliveryAccount> deliveryLoginService;
+        public LoginController(GenericLoginService<Account> accountLoginService,
+                                GenericLoginService<MerchantAccount> merchantLoginService,
+                                GenericLoginService<DeliveryAccount> deliveryLoginService)
+        {
+            this.accountLoginService = accountLoginService;
+            this.merchantLoginService = merchantLoginService;
+            this.deliveryLoginService = deliveryLoginService;
+        }
 
-
-		[HttpPost("Merchant")]
-		public async Task<IActionResult> LoginMerchant(CreateLoginDTO createLoginDTO)
-		{
-			return Ok(await merchantLoginService.getTokenEmployee(createLoginDTO, account => account.Role));
-		}
+        [HttpPost("Employee")]
+        public async Task<IActionResult> LoginEmployee(CreateLoginDTO createLoginDTO)
+        {
+            return Ok(await accountLoginService.getTokenEmployee(createLoginDTO, account => account.Role));
+        }
 
 
+        [HttpPost("Merchant")]
+        public async Task<IActionResult> LoginMerchant(CreateLoginDTO createLoginDTO)
+        {
+            return Ok(await merchantLoginService.getTokenEmployee(createLoginDTO, account => account.Role));
+        }
 
-		[HttpPost("Delivery")]
-		public async Task<IActionResult> LoginDelivery(CreateLoginDTO createLoginDTO)
-		{
-			return Ok(await deliveryLoginService.getTokenEmployee(createLoginDTO, account => account.Role));
-		}
-	}
+
+
+        [HttpPost("Delivery")]
+        public async Task<IActionResult> LoginDelivery(CreateLoginDTO createLoginDTO)
+        {
+            return Ok(await deliveryLoginService.getTokenEmployee(createLoginDTO, account => account.Role));
+        }
+    }
 }
